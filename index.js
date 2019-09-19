2d911b93eb6f4945ffab29a9bb21d8d647f084be
viniceosm <miiller.vini@gmail.com>
2019-09-19 13:51:30 -0300

    Adicionando estilo

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function setwebcam() {
 
 function setwebcam2(options) {
     console.log(options);
-    document.getElementById("result").innerHTML = "- scanning -";
+    document.getElementById("result").innerHTML = "scanning";
     if (stype == 1) {
         setTimeout(captureToCanvas, 500);
         return;
